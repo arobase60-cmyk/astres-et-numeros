2b0fc9335b1b30d03e3d81174290fb9611ea8a44
arobase60-cmyk <arobase60@gmail.com>
2026-08-15 17:58:34 +0200

    Ajout de l integration sitemap pour le SEO Google

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
   site: 'http://astresnumeros.fr',
-  
   trailingSlash: 'always',
+  integrations: [sitemap()],
 });
